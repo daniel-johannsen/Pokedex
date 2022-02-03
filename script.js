b9a366441a81ea7222818f7fd663b8ffb607287a
aniel Johannsen <daniel-johannsen@web.de>
2022-02-03 23:28:50 +0100

    js functions

--- script.js
+++ script.js
@@ -1,22 +1,64 @@
 let currentPokemon;
+let allPokemon = [];
+let pokemonNumber;
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/pikachu';
+    let url = 'https://pokeapi.co/api/v2/pokemon/';
     let response = await fetch(url);
-    currentPokemon = await response.json();
-
-    console.log('Loaded Pokemon:', currentPokemon);
+    let apiAsJson = await response.json();
+    allPokemon = apiAsJson['results'];
+    console.log('Loaded Pokemon:', allPokemon);
 
     renderPokemonInfo();
 }
 
-function renderPokemonInfo() {
-    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    renderPokemonImage();
+async function renderPokemonInfo() {
+    for (let i = 0; i < allPokemon.length; i++) {
+        pokemonNumber = allPokemon[i];
+        let newUrl = pokemonNumber['url'];
+        let response = await fetch(newUrl);
+        currentPokemon = await response.json();
+        createInfoBox(currentPokemon);
+    }
 }
 
-function renderPokemonImage() {
-    let image = document.createElement('img');
-    image.src = currentPokemon['sprites']['front_shiny'];
-    document.getElementById('img').appendChild(image);
+function createInfoBox(currentPokemon) {
+    document.getElementById('pokedex').innerHTML += `
+    <div id="box${pokemonNumber}" onclick="showStats(${currentPokemon})">
+        <div class="name">${currentPokemon['name']}</div>
+        <div class="type"></div>
+        <div class="img">${currentPokemon['sprites']['front_shiny']}</div>
+        <div class="stats_top">Stats</div>
+        <div class="stats d-none" id="stats"></div>
+    </div>
+    `;
+}
+
+function showStats(currentPokemon) {
+    document.getElementById('stats').innerHTML += `
+    <div class="stats_element">
+        <div class="stats_left"></div>
+        <div class="stats_right"></div>
+    </div>
+    <div class="stats_element">
+        <div class="stats_left"></div>
+        <div class="stats_right"></div>
+    </div>
+    <div class="stats_element">
+        <div class="stats_left"></div>
+        <div class="stats_right"></div>
+    </div>
+    <div class="stats_element">
+        <div class="stats_left"></div>
+        <div class="stats_right"></div>
+    </div>
+    <div class="stats_element">
+        <div class="stats_left"></div>
+        <div class="stats_right"></div>
+    </div>
+    <div class="stats_element">
+        <div class="stats_left"></div>
+        <div class="stats_right"></div>
+    </div>
+    `;
 }
