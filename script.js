7492388f9ac194807804821b703698142090fec7
aniel Johannsen <daniel-johannsen@web.de>
2022-02-20 21:48:50 +0100

    js functions and css

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let currentPokemon;
 let allPokemon = [];
-let pokemonNumber;
+let pokemonNames = [];
+let dataList = document.getElementById('pokemon');
 
 async function loadPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon/';
@@ -14,51 +15,77 @@ async function loadPokemon() {
 
 async function renderPokemonInfo() {
     for (let i = 0; i < allPokemon.length; i++) {
-        pokemonNumber = allPokemon[i];
-        let newUrl = pokemonNumber['url'];
+        let element = allPokemon[i];
+        let newUrl = element['url'];
         let response = await fetch(newUrl);
         currentPokemon = await response.json();
-        createInfoBox(currentPokemon);
+        console.log('currentPokemon', currentPokemon);
+        createInfoBox(i);
+        pokemonNames.push(currentPokemon['name']);
     }
 }
 
-function createInfoBox(currentPokemon) {
+pokemonNames.forEach(function(item) {
+    let option = document.createElement('option');
+    option.value = item;
+    dataList.appendChild(option);
+});
+
+function createInfoBox(i) {
     document.getElementById('pokedex').innerHTML += `
-    <div id="box${pokemonNumber}" onclick="showStats(${currentPokemon})">
-        <div class="name">${currentPokemon['name']}</div>
-        <div class="type"></div>
-        <div class="img">${currentPokemon['sprites']['front_shiny']}</div>
-        <div class="stats_top">Stats</div>
-        <div class="stats d-none" id="stats"></div>
+    <div id="box${i}" class="box zoom bg_${currentPokemon['types'][0]['type']['name']}" onclick="showStats(${i})">
+        <div class="name">${capitalizeFirstLetter(currentPokemon['name'])}</div>
+        <div class="img"><img src="${currentPokemon['sprites']['front_shiny']}"></div>
+        <div class="type">${currentPokemon['types'][0]['type']['name']}</div>
+        <div class="stats_top borders_bottom" id="statsTop${i}">Stats</div>
+        <div class="stats borders_bottom d-none" id="stats${i}">
+            <div class="stats_element">
+                <div class="stats_left">${currentPokemon['stats'][0]['stat']['name']}</div>
+                <div class="stats_right"><div id="progressBar" class="progress-bar" role="progressbar" style="width: ${currentPokemon['stats'][0]['base_stat']}%;">${currentPokemon['stats'][0]['base_stat']}%</div></div>
+            </div>
+            <div class="stats_element">
+                <div class="stats_left">${currentPokemon['stats'][1]['stat']['name']}</div>
+                <div class="stats_right"><div id="progressBar" class="progress-bar" role="progressbar" style="width: ${currentPokemon['stats'][1]['base_stat']}%;">${currentPokemon['stats'][1]['base_stat']}%</div></div>
+            </div>
+            <div class="stats_element">
+                <div class="stats_left">${currentPokemon['stats'][2]['stat']['name']}</div>
+                <div class="stats_right"><div id="progressBar" class="progress-bar" role="progressbar" style="width: ${currentPokemon['stats'][2]['base_stat']}%;">${currentPokemon['stats'][2]['base_stat']}%</div></div>
+            </div>
+            <div class="stats_element">
+                <div class="stats_left">${currentPokemon['stats'][3]['stat']['name']}</div>
+                <div class="stats_right"><div id="progressBar" class="progress-bar" role="progressbar" style="width: ${currentPokemon['stats'][3]['base_stat']}%;">${currentPokemon['stats'][3]['base_stat']}%</div></div>
+            </div>
+            <div class="stats_element">
+                <div class="stats_left">${currentPokemon['stats'][4]['stat']['name']}</div>
+                <div class="stats_right"><div id="progressBar" class="progress-bar" role="progressbar" style="width: ${currentPokemon['stats'][4]['base_stat']}%;">${currentPokemon['stats'][4]['base_stat']}%</div></div>
+            </div>
+            <div class="stats_element">
+                <div class="stats_left">${currentPokemon['stats'][5]['stat']['name']}</div>
+                <div class="stats_right"><div id="progressBar" class="progress-bar" role="progressbar" style="width: ${currentPokemon['stats'][5]['base_stat']}%;">${currentPokemon['stats'][5]['base_stat']}%</div></div>
+            </div>
+        </div>
     </div>
     `;
 }
 
-function showStats(currentPokemon) {
-    document.getElementById('stats').innerHTML += `
-    <div class="stats_element">
-        <div class="stats_left"></div>
-        <div class="stats_right"></div>
-    </div>
-    <div class="stats_element">
-        <div class="stats_left"></div>
-        <div class="stats_right"></div>
-    </div>
-    <div class="stats_element">
-        <div class="stats_left"></div>
-        <div class="stats_right"></div>
-    </div>
-    <div class="stats_element">
-        <div class="stats_left"></div>
-        <div class="stats_right"></div>
-    </div>
-    <div class="stats_element">
-        <div class="stats_left"></div>
-        <div class="stats_right"></div>
-    </div>
-    <div class="stats_element">
-        <div class="stats_left"></div>
-        <div class="stats_right"></div>
-    </div>
-    `;
+function showStats(i) {
+    document.getElementById(`stats${i}`).classList.toggle('d-none');
+    document.getElementById(`statsTop${i}`).classList.toggle('borders_bottom');
 }
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// function filterNames() {
+//     let input = document.getElementById('search').value;
+//     input = input.toLowerCase();
+
+//     for (let j = 0; j < pokemonNames.length; j++) {
+//         let name = pokemonNames[j];
+
+//         if (!name.includes(input)) {
+//             document.getElementById(`box${j}`).classList.add('d-none');
+//         }
+//     }
+// }
